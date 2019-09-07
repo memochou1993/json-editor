58ca4efd24f93c506de3dc9cc72a8b5ee82def60
Memo Chou <memochou1993@gmail.com>
2019-09-08 02:40:41 +0800

    Change default color theme

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import colors from 'vuetify/es5/util/colors';
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
 
@@ -10,7 +9,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.indigo,
+        primary: '#061D30',
       },
     },
   },
