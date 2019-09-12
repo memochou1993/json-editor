b0d0f545a09e6c9c1294f3e3c96ce142fbe628c7
Memo Chou <memochou1993@gmail.com>
2019-09-12 22:33:58 +0800

    Change favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>JSON Editor</title>
+        <link rel="icon" href="favicon.png">
     </head>
     <body>
         <div id="app"></div>
