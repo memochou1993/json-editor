06aa4c85050975a76eca15dd28f1c698cd73c085
Memo Chou <memochou1993@gmail.com>
2019-09-15 02:43:47 +0800

    Add records state in Vuex record module

--- resources/js/store/modules/record.js
+++ resources/js/store/modules/record.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import Common from '@/helpers/Common';
+import Storage from '@/helpers/Storage';
 
 export default {
   namespaced: true,
   state: {
+    records: Storage.get('records') || [],
     record: null,
   },
   mutations: {
+    setRecords(state, records) {
+      state.records = records;
+    },
     setRecord(state, record) {
       state.record = record;
     },
@@ -17,8 +22,16 @@ export default {
     }) {
       commit('setRecord', null);
     },
-    fetchRecord({
+    setRecords({
       commit,
+    }, records) {
+      commit('setRecords', records);
+      Storage.set('records', records);
+    },
+    fetchRecord({
+      state,
+      commit,
+      dispatch,
     }, code) {
       commit('setLoading', true, { root: true });
       return new Promise((resolve, reject) => {
@@ -29,6 +42,7 @@ export default {
             resolve(data);
           })
           .catch((error) => {
+            dispatch('setRecords', state.records.filter(record => record.code !== code));
             reject(error);
           })
           .finally(() => {
@@ -37,13 +51,16 @@ export default {
       });
     },
     storeRecord({
+      state,
       commit,
+      dispatch,
     }, params) {
       commit('setLoading', true, { root: true });
       return new Promise((resolve, reject) => {
         axios.post('/records', params)
           .then(async ({ data }) => {
             await Common.defer(0.25);
+            dispatch('setRecords', [...state.records, data]);
             commit('setRecord', data);
             resolve(data);
           })
@@ -59,12 +76,16 @@ export default {
     updateRecord({
       state,
       commit,
+      dispatch,
     }, params) {
       commit('setLoading', true, { root: true });
       return new Promise((resolve, reject) => {
         axios.put(`/records/${state.record.code}`, params)
           .then(async ({ data }) => {
             await Common.defer(0.25);
+            dispatch('setRecords', state.records.find(record => record.code === data.code)
+              ? state.records.map(record => record.code === data.code ? data : record)
+              : [...state.records, data]);
             commit('setRecord', data);
             resolve(data);
           })
@@ -80,12 +101,14 @@ export default {
     destroyRecord({
       state,
       commit,
+      dispatch,
     }) {
       commit('setLoading', true, { root: true });
       return new Promise((resolve, reject) => {
         axios.delete(`/records/${state.record.code}`)
           .then(async () => {
             await Common.defer(0.25);
+            dispatch('setRecords', state.records.filter(record => record.code !== state.record.code));
             commit('setRecord', null);
             resolve();
           })
