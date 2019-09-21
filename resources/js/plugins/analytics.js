d23b98c6d1546e1920d9f741d4e7ccad6d3a9134
Memo Chou <memochou1993@gmail.com>
2019-09-21 12:40:14 +0800

    Modify analytics plugin

--- resources/js/plugins/analytics.js
+++ resources/js/plugins/analytics.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
 import router from '@/router';
 
+const id = process.env.MIX_GOOGLE_ANALYTICS_ID;
+
 Vue.use(VueAnalytics, {
-  id: process.env.MIX_GOOGLE_ANALYTICS_ID,
+  id: id || 'N/A',
+  disabled: !id,
   router,
   debug: {
     sendHitTask: process.env.NODE_ENV === 'production',
